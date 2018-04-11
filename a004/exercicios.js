6a80c6d9a7d0582f4483cb8159534a1bcefb2cc9
Andre Bronca <bronca.andre@gmail.com>
2018-04-11 09:57:33 -0300

    retornos de funcoes com array e objeto

--- a004/exercicios.js
+++ a004/exercicios.js
@@ -1,11 +1,26 @@
 var isTruthy = function( v ){
     return !!v;
-}
+};
 
+//todos os valores false
 isTruthy('');
 isTruthy(undefined);
-isTruthy('a');
+isTruthy(false);
+isTruthy(null);
+isTruthy(0);
+isTruthy(-0);
+isTruthy(NaN);
+isTruthy(1 - 1);
+isTruthy(2 * 0);
+
+//valores verdadeiros
 isTruthy(1);
+isTruthy('a');
+isTruthy([]);
+isTruthy({});
+isTruthy(function(){});
+isTruthy([1,3,5]);
+isTruthy({"um": 1, "dois": 2});
 
 var carro = {
     marca: 'VW',
@@ -20,23 +35,23 @@ var carro = {
 
 carro.mudaCor = function(c){
     this.cor = c;
-}
+};
 
 carro.obterCor = function(){
     return this.cor;
-}
+};
 
 carro.obterModelo = function(){
     return this.modelo;
-}
+};
 
 carro.obterMarca = function(){
     return this.marca;
-}
+};
 
 carro.obterMarcaModelo = function(){
     return 'Esse carro é um '+ this.obterMarca() +' '+ this.obterModelo();
-}
+};
 
 carro.adicionarPessoas = function( qtdPessoas ){
     var vagas = this.assentos - this.quantidadePessoas;
@@ -50,7 +65,7 @@ carro.adicionarPessoas = function( qtdPessoas ){
     } else if (vagas > 0  && qtdPessoas > vagas){
         return 'Só cabem mais '+ vagas + strPessoas;
     }
-}
+};
 
 carro.obterCor();
 carro.mudaCor('vermelho');
